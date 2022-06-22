61405e44a3cfe42b8f25123666a61ffdc1bfd813
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-23 00:05:39 +0100

    Create Create-Shortcut.psm1

---
+++ CustomModules/Create-Shortcut/Create-Shortcut.psm1
@@ -0,0 +1,28 @@
+﻿<#
+  .Description
+   Creates a shortcut in the user's desktop directory of the specified program
+#>
+
+function Create-Shortcut
+{
+    param
+    (
+        [string]${ProgramName},
+        [string]${ShortcutPath},
+        [string]${TargetPath}
+    )
+
+    try
+    {
+        ${WSScriptObj} = New-Object -ComObject ("WScript.Shell")
+        ${Shortcut} = ${WSScriptObj}.CreateShortcut(${ShortcutPath})
+        ${Shortcut}.TargetPath = "${TargetPath}\chrome-win\chrome.exe"
+        ${Shortcut}.Save()
+    }
+    catch
+    {
+        throw $_.Exception.Message
+    }
+}
+
+Export-ModuleMember -Function Create-Shortcut
