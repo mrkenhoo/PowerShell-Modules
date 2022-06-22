d9f5fdafb6a3304c9df3fc74d65d8d3602adaf57
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-23 00:05:37 +0100

    Create Unzip-File.psm1

---
+++ CustomModules/Unzip-File/Unzip-File.psm1
@@ -0,0 +1,23 @@
+﻿<#
+  .Description
+   Extracts the specified file to a directory
+#>
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function Unzip-File
+{
+    #
+    # Function source:
+    # 'https://stackoverflow.com/questions/27768303/how-to-unzip-a-file-in-powershell'
+    #
+    param
+    (
+        [string]${SourceFile},
+        [string]${DestinationPath}
+    )
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory(${SourcePath}, ${DestinationFile})
+}
+
+Export-ModuleMember Unzip-File
