96640787f1b9e8aef268d04774cdbac828cbdcb0
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-23 00:05:35 +0100

    Create Modules-Installer.ps1

---
+++ Modules-Installer.ps1
@@ -0,0 +1,11 @@
+﻿#Requires -RunAsAdministrator
+
+${ModulesList} = @(Get-ChildItem -Directory .\CustomModules)
+
+${ModulesPath} = "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules"
+
+ForEach (${Module} in ${ModulesList})
+{
+    Copy-Item -LiteralPath .\CustomModules\${Module} -Confirm -Destination ${ModulesPath} -Force -Recurse | Out-Null
+    Import-Module -Name ${Module}
+}
