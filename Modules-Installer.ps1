bfa4582145fe481ed7c68276e0c4c3bca210b50d
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-27 12:49:20 +0100

    Wait for user input to exit

--- Modules-Installer.ps1
+++ Modules-Installer.ps1
@@ -1,11 +1,12 @@
 ﻿#Requires -RunAsAdministrator
 
-${ModulesList} = @(Get-ChildItem -Directory .\CustomModules)
-
 ${ModulesPath} = "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules"
 
-ForEach (${Module} in ${ModulesList})
+ForEach (${Module} in @(Get-ChildItem -Directory .\CustomModules))
 {
     Copy-Item -LiteralPath .\CustomModules\${Module} -Confirm -Destination ${ModulesPath} -Force -Recurse | Out-Null
     Import-Module -Name ${Module}
 }
+
+Write-Host -NoNewLine 'Press any key to continue...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
