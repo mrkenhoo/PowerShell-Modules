0dcb58a3d90cd96d150306871cdd3e30c421b61b
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-27 14:10:13 +0100

    Add parameter -Install, improved code

--- Modules-Installer.ps1
+++ Modules-Installer.ps1
@@ -1,12 +1,48 @@
 ﻿#Requires -RunAsAdministrator
+param
+(
+    [Parameter(Mandatory=$true)]
+    [bool]${Install}
+)
 
-${ModulesPath} = "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules"
-
-ForEach (${Module} in @(Get-ChildItem -Directory .\CustomModules))
+process
 {
-    Copy-Item -LiteralPath .\CustomModules\${Module} -Confirm -Destination ${ModulesPath} -Force -Recurse | Out-Null
-    Import-Module -Name ${Module}
-}
+    try
+    {
+        if (${Install})
+        {
+            ForEach (${Module} in @(Get-ChildItem -Directory .\CustomModules))
+            {
+                Copy-Item -LiteralPath ".\CustomModules\${Module}" -Confirm -Destination "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules" -Force -Recurse | Out-Null
+                Import-Module -Name ${Module}
+            }
 
-Write-Host -NoNewLine 'Press any key to continue...'
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+            Write-Host -NoNewLine 'Press any key to continue...'
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+        }
+        else
+        {
+            ForEach (${Module} in @(Get-ChildItem -Directory ".\CustomModules"))
+            {
+                if (Test-Path -Path "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules\${Module}")
+                {
+                    Remove-Item -Path "${env:SystemRoot}\System32\WindowsPowerShell\v1.0\Modules\${Module}" -Confirm -Force -Recurse | Out-Null
+                    Remove-Module -Name "${Module}"
+                }
+                else
+                {
+                    Write-Warning -Message "The module '${Module}' is not installed"
+                }
+            }
+
+            Write-Host -NoNewLine 'Press any key to continue...'
+            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+        }
+    }
+    catch
+    {
+        $_.Message.Exception
+        Write-Host -NoNewLine 'Press any key to continue...'
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    }
+}
